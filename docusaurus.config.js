e1bc7f643ff30b2833a22b4e2b2dde9f7adeb0f8
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2025-11-15 21:25:12 -0700

    blud forgot he is gonna use a custom domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
     url: 'https://docs.penguinmod.com',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/PenguinMod-Docs/',
+    baseUrl: '/',
     trailingSlash: true,
     
     // GitHub pages deployment config.
